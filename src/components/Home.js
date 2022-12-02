cc5247c32a5a5fbb5de31a24d757c1594ff56d27
yugsharma4 <yugsharma422@gmail.com>
2022-12-02 11:09:56 +0530

    Initial setup

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Jumbotron, Button } from 'reactstrap';
+
+
+const Home = () => {
+  return (
+    <div>
+      <div className='jumbotron'>
+      <h1 className="display-3">I'm Developer!</h1>
+        <h2 className="display">Yug Sharma</h2>
+        <p></p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
